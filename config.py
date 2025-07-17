441849aaed79ebc5ff6d016b5819cb97a806629e
Crow <122665624+FrogLegs21@users.noreply.github.com>
2025-07-17 13:02:10 -0700

    config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Discord Bot Credentials
+CLIENT_ID = "Your Client ID"
+CLIENT_SECRET = "Your Client Secret"
+REDIRECT_URI = "http://localhost:5000/callback"
+BOT_TOKEN = "Your Bot Token"
+OAUTH_SCOPE = "identify guilds"
+DB_FILE = "temproles.db"
+
+# Channels
+LOG_CHANNEL_ID = Your channel ID  # Replace with your actual log channel ID
+channel_id = Your channel ID  # Replace with your actual log channel ID
+APPROVAL_CHANNEL_ID = 1373933113390006389  # Replace with your actual log channel ID
+
+# EXAMPLE
+# LOG_CHANNEL_ID = 1373933113390006389  # Replace with your actual log channel ID
